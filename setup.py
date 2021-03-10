3c4e38c939a511f7374eab5393f8750ebbd003b3
lgarzio <lgarzio@marine.rutgers.edu>
2021-03-10 12:37:57 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='phglider',
+    version='1.0',
+    packages=find_packages(),
+    url='https://github.com/lgarzio/phglider',
+    author='Lori Garzio',
+    author_email='lgarzio@marine.rutgers.edu',
+    description='Rutgers University Center for Ocean Observing Leadership: tools for processing and plotting glider'
+                'pH data.'
+)
