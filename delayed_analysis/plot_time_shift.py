95a3116196465116f47a5f78ef223b6e6539799a
Lori Garzio <lgarzio@marine.rutgers.edu>
2021-04-03 22:31:27 -0400

    apply and plot time shifts

---
+++ delayed_analysis/plot_time_shift.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+"""
+Author: Lori Garzio on 4/3/2021
+Last modified: 4/3/2021
+Plot raw and time-shifted glider pH and oxygen data at user-specified intervals to evaluate the applied time shift.
+"""
+
+import os
+import pandas as pd
+import xarray as xr
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+plt.rcParams.update({'font.size': 16})
+pd.set_option('display.width', 320, "display.max_columns", 10)  # for display in pycharm console
+
+
+def plot_scatter(figure, axis, x, y, color, plt_ttl, cmin=None, cmax=None):
+    if cmin:
+        xc = axis.scatter(x, y, c=color, cmap=cm.jet, s=10, edgecolor='None', vmin=cmin, vmax=cmax)
+    else:
+        xc = axis.scatter(x, y, c=color, cmap=cm.jet, s=10, edgecolor='None')
+
+    # format colorbar
+    divider = make_axes_locatable(axis)
+    cax = divider.new_horizontal(size='5%', pad=0.1, axes_class=plt.Axes)
+    figure.add_axes(cax)
+    cb = plt.colorbar(xc, cax=cax)
+
+    # add y-axis label
+    axis.set_ylabel('Depth (m)')
+
+    # format x-axis
+    xfmt = mdates.DateFormatter('%m-%d\n%H:%M')
+    axis.xaxis.set_major_formatter(xfmt)
+    axis.xaxis.set_tick_params(labelsize=15)
+
+    axis.set_title(plt_ttl, fontsize=18)
+
+
+def main(fname, plt_interval):
+    ds = xr.open_dataset(fname)
+    plotting_vars = [['ph_total', 'ph_total_shifted'],
+                ['oxygen_concentration', 'oxygen_concentration_shifted']]
+
+    sdir = os.path.join(os.path.dirname(fname), 'timeshift_figs_test')
+    os.makedirs(sdir, exist_ok=True)
+
+    daterange = pd.date_range(ds.time.values[0], ds.time.values[-1], freq=plt_interval)
+    for dt_idx, dr in enumerate(daterange):
+        if dt_idx > 0:
+            ds_sel = ds.sel(time=slice(daterange[dt_idx - 1], dr))
+            for plt_vars in plotting_vars:
+                fig, axs = plt.subplots(2, figsize=(12, 12), sharex=True, sharey=True)
+                for i, ax in enumerate(fig.axes):
+                    if i == 0:
+                        plot_scatter(fig, ax, ds_sel.time.values, ds_sel.depth.values, ds_sel[plt_vars[0]].values,
+                                     plt_vars[0])
+                    else:
+                        plot_scatter(fig, ax, ds_sel.time.values, ds_sel.depth.values, ds_sel[plt_vars[1]].values,
+                                     plt_vars[1])
+
+                ax.invert_yaxis()
+
+                sname = os.path.join(sdir, '{}_{}.png'.format(plt_vars[0], daterange[dt_idx - 1].strftime('%Y%m%dT%H%M')))
+                plt.savefig(sname, dpi=300)
+                plt.close()
+
+
+if __name__ == '__main__':
+    ncfile = '/Users/garzio/Documents/rucool/Saba/gliderdata/2021/ru30-20210226T1647/delayed/ru30-20210226T1647-profile-sci-delayed_shifted.nc'
+    plot_interval = '3H'  # 3 hours
+    main(ncfile, plot_interval)
--- delayed_analysis/time_shift.py
+++ delayed_analysis/time_shift.py
@@ -2,27 +2,19 @@
 
 """
 Author: Lori Garzio on 4/2/2021
-Last modified: 4/2/2021
+Last modified: 4/3/2021
+Apply a best time shift for pH and oxygen glider data. Before a shift is applied, the dataset is recalculated to
+one second averages. The time shifted data are added back to the dataset which is saved as a new netCDF file.
 """
 
-import os
 import numpy as np
 import pandas as pd
 import datetime as dt
 import json
 import xarray as xr
-import time
-from erddapy import ERDDAP
-import cmocean as cmo
-import matplotlib.cm as cm
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 import functions.phcalc as phcalc
 import functions.common as cf
-plt.rcParams.update({'font.size': 16})
 pd.set_option('display.width', 320, "display.max_columns", 10)  # for display in pycharm console
-
 
 
 def main(deploy, sensor_sn, shifts, fname):
@@ -48,18 +40,17 @@ def main(deploy, sensor_sn, shifts, fname):
         if val > 0:  # if there is a shift calculated
             if key == 'ph':
                 varname = 'sbe41n_ph_ref_voltage'
-                ph_shiftname = '{}_shifted_{}s'.format(varname, val)
+                ph_shiftname = '{}_shifted'.format(varname)
             elif key == 'oxygen':
                 varname = 'oxygen_concentration'
-                oxy_shiftname = '{}_shifted_{}s'.format(varname, val)
             da = pd.DataFrame(df[varname])
             tm_shift = df.index - dt.timedelta(seconds=val)
             da['time_shift'] = tm_shift
             da.reset_index(drop=True, inplace=True)  # drop the original time index
-            da = da.rename(columns={varname: '{}_shifted_{}s'.format(varname, val), 'time_shift': 'time'})
+            da = da.rename(columns={varname: '{}_shifted'.format(varname), 'time_shift': 'time'})
             da.set_index('time', inplace=True)
             df = df.merge(da, how='left', left_index=True, right_index=True)
-            col_subset.append('{}_shifted_{}s'.format(varname, val))
+            col_subset.append('{}_shifted'.format(varname))
 
     df = df.dropna(axis=0, how='all', subset=col_subset)
 
@@ -89,10 +80,10 @@ def main(deploy, sensor_sn, shifts, fname):
         try:
             ds_shifted[varname] = ds_shifted[varname].assign_attrs(ds[varname].attrs)
         except KeyError:
-            if 'sbe41n_ph_ref_voltage' in varname:
+            if varname == 'sbe41n_ph_ref_voltage':
                 ds_shifted[varname] = ds_shifted[varname].assign_attrs(ds['sbe41n_ph_ref_voltage'].attrs)
                 ds_shifted[varname].attrs['comment'] = 'Values time shifted by -{} seconds to correct for thermal lag.'.format(sensor_shifts['ph'])
-            if 'oxygen_concentration' in varname:
+            if varname == 'oxygen_concentration_shifted':
                 ds_shifted[varname] = ds_shifted[varname].assign_attrs(ds['oxygen_concentration'].attrs)
                 ds_shifted[varname].attrs['comment'] = 'Values time shifted by -{} seconds to correct for thermal lag.'.format(sensor_shifts['oxygen'])
             if 'sci_water_pressure' in varname:
@@ -101,7 +92,7 @@ def main(deploy, sensor_sn, shifts, fname):
                     del ds_shifted[varname].attrs[item]
                 ds_shifted[varname].attrs['observation_type'] = 'calculated'
                 ds_shifted[varname].attrs['units'] = 'dbar'
-            if 'f_p' in varname:
+            if varname == 'f_p':
                 ds_shifted[varname].attrs['observation_type'] = 'calculated'
                 ds_shifted[varname].attrs['comment'] = 'Polynomial evaluation of sensor pressure response polynomial coefficients (f6-f1) and pressure_dbar'
                 ds_shifted[varname].attrs['f6'] = cc['f6']
@@ -110,10 +101,10 @@ def main(deploy, sensor_sn, shifts, fname):
                 ds_shifted[varname].attrs['f3'] = cc['f3']
                 ds_shifted[varname].attrs['f2'] = cc['f2']
                 ds_shifted[varname].attrs['f1'] = cc['f1']
-                ds_shifted[varname].attrs['units'] = 1
+                ds_shifted[varname].attrs['units'] = '1'
             if varname == 'ph_total':
                 ds_shifted[varname].attrs['observation_type'] = 'calculated'
-                ds_shifted[varname].attrs['units'] = 1
+                ds_shifted[varname].attrs['units'] = '1'
                 ds_shifted[varname].attrs['k0'] = cc['k0']
                 ds_shifted[varname].attrs['k2'] = cc['k2']
                 ds_shifted[varname].attrs['comment'] = 'pH calculated on the total scale using pH reference voltage, pressure_dbar ,' \
@@ -121,7 +112,7 @@ def main(deploy, sensor_sn, shifts, fname):
                                                        'polynomial coefficients.'
             if varname == 'ph_total_shifted':
                 ds_shifted[varname].attrs['observation_type'] = 'calculated'
-                ds_shifted[varname].attrs['units'] = 1
+                ds_shifted[varname].attrs['units'] = '1'
                 ds_shifted[varname].attrs['k0'] = cc['k0']
                 ds_shifted[varname].attrs['k2'] = cc['k2']
                 ds_shifted[varname].attrs['comment'] = 'pH calculated on the total scale using time shifted pH reference voltage, pressure_dbar, ' \
@@ -135,66 +126,10 @@ def main(deploy, sensor_sn, shifts, fname):
     ds_shifted = ds_shifted.assign_attrs(ds_global_attrs)
     ds_shifted.to_netcdf('{}_shifted.nc'.format(fname.split('.')[0]))
 
-    # plot
-    # df.reset_index(inplace=True)
-    # daterange = pd.date_range(df.time[0], df.time[len(df) - 1], freq='3H')
-    # for dt_idx, dr in enumerate(daterange):
-    #     if dt_idx > 0:
-    #         df_dr = df[(pd.to_datetime(df.time) < dr) & (pd.to_datetime(df.time) >= daterange[dt_idx - 1])]
-    #         for pv in ['ph', 'oxygen']:
-    #             if pv == 'ph':
-    #                 colname = 'ph_total'
-    #                 colname_shift = 'ph_total_shifted'
-    #             else:
-    #                 colname = 'oxygen_concentration'
-    #                 colname_shift = oxy_shiftname
-    #
-    #             fig, (ax1, ax2) = plt.subplots(2, figsize=(12, 12), sharex=True, sharey=True)
-    #             # xc = ax1.scatter(tm, depth, c=oxy, cmap=cmo.cm.oxy, s=10, edgecolor='None')
-    #             # if pv == 'ph':
-    #             #     xc = ax1.scatter(df_dr.time, df_dr.depth, c=df_dr[colname], cmap=cm.jet, s=10, edgecolor='None',
-    #             #                      vmin=7.92, vmax=8.19)
-    #             # else:
-    #             #     xc = ax1.scatter(df_dr.time, df_dr.depth, c=df_dr[colname], cmap=cm.jet, s=10, edgecolor='None')
-    #             xc = ax1.scatter(df_dr.time, df_dr.depth, c=df_dr[colname], cmap=cm.jet, s=10, edgecolor='None')
-    #
-    #             # format colorbar
-    #             divider = make_axes_locatable(ax1)
-    #             cax = divider.new_horizontal(size='5%', pad=0.1, axes_class=plt.Axes)
-    #             fig.add_axes(cax)
-    #             cb = plt.colorbar(xc, cax=cax)
-    #
-    #             ax1.set_title('{} - no shift'.format(pv), fontsize=18)
-    #             ax1.invert_yaxis()
-    #
-    #             # xc = ax2.scatter(tm, depth, c=oxy_shift, cmap=cmo.cm.oxy, s=10, edgecolor='None')
-    #             # if pv == 'ph':
-    #             #     xc = ax2.scatter(df_dr.time, df_dr.depth, c=df_dr[colname_shift], cmap=cm.jet, s=10, edgecolor='None',
-    #             #                      vmin=7.92, vmax=8.19)
-    #             # else:
-    #             #     xc = ax2.scatter(df_dr.time, df_dr.depth, c=df_dr[colname_shift], cmap=cm.jet, s=10, edgecolor='None')
-    #             xc = ax2.scatter(df_dr.time, df_dr.depth, c=df_dr[colname_shift], cmap=cm.jet, s=10, edgecolor='None')
-    #
-    #             # format colorbar
-    #             divider = make_axes_locatable(ax2)
-    #             cax = divider.new_horizontal(size='5%', pad=0.1, axes_class=plt.Axes)
-    #             fig.add_axes(cax)
-    #             cb = plt.colorbar(xc, cax=cax)
-    #
-    #             ax2.set_title('{} - {} second shift'.format(pv, sensor_shifts[pv]), fontsize=18)
-    #
-    #             xfmt = mdates.DateFormatter('%m-%dT%H:%M')
-    #             ax2.xaxis.set_major_formatter(xfmt)
-    #             ax2.xaxis.set_tick_params(labelsize=15)
-    #
-    #             sname = os.path.join(sdir, '{}_{}.png'.format(pv, daterange[dt_idx - 1].strftime('%Y%m%dT%H%M')))
-    #             plt.savefig(sname, dpi=300)
-    #             plt.close()
-
 
 if __name__ == '__main__':
     deployment = 'ru30-20210226T1647'
     ph_sn = 'sbe10344'
-    sensor_shifts = {'ph': 31, 'oxygen': 36}  # sensor_shifts = {'ph': 31, 'oxygen': 36}
+    sensor_shifts = {'ph': 31, 'oxygen': 36}  # sensor_shifts = {'ph': 0, 'oxygen': 36}
     ncfile = '/Users/garzio/Documents/rucool/Saba/gliderdata/2021/ru30-20210226T1647/delayed/ru30-20210226T1647-profile-sci-delayed.nc'
     main(deployment, ph_sn, sensor_shifts, ncfile)
