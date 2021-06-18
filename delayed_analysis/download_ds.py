15b7720ec3f3009907cab58d752f128d6b606e82
Lori Garzio <lgarzio@marine.rutgers.edu>
2021-06-18 14:31:08 -0400

    Update download_ds.py

--- delayed_analysis/download_ds.py
+++ delayed_analysis/download_ds.py
@@ -2,8 +2,8 @@
 
 """
 Author: Lori Garzio on 4/2/2021
-Last modified: 4/2/2021
-Download a profile-sci-delayed netCDF dataset and save to a local directory
+Last modified: 6/18/2021
+Download a user-specified netCDF dataset from RUCOOL's glider ERDDAP server and save to a local directory
 """
 
 import os
@@ -11,17 +11,17 @@ import functions.common as cf
 
 
 def main(deploy, sdir):
+    os.makedirs(sdir, exist_ok=True)
     ru_server = 'http://slocum-data.marine.rutgers.edu//erddap'
-    glider_id = '{}-profile-sci-delayed'.format(deploy)
     glider_vars = ['latitude', 'longitude', 'depth', 'conductivity', 'salinity', 'sci_water_pressure',
                    'temperature', 'sbe41n_ph_ref_voltage', 'chlorophyll_a', 'oxygen_concentration', 'water_depth',
                    'profile_lat', 'profile_lon', 'profile_time']
-    ds = cf.get_erddap_dataset(ru_server, glider_id, glider_vars)
-    fname = '{}-profile-sci-delayed.nc'.format(deployment)
+    ds = cf.get_erddap_dataset(ru_server, deploy, glider_vars)
+    fname = f'{deploy}.nc'
     ds.to_netcdf(os.path.join(sdir, fname))
 
 
 if __name__ == '__main__':
-    deployment = 'ru30-20210226T1647'
-    savedir = '/Users/garzio/Documents/rucool/Saba/gliderdata/2021/ru30-20210226T1647/delayed'
+    deployment = 'ru30-20210503T1929-profile-sci-delayed'
+    savedir = '/Users/garzio/Documents/rucool/Saba/gliderdata/2021/ru30-20210503T1929/delayed'
     main(deployment, savedir)
