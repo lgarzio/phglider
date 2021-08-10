fd0332910f394bdb5afe996844692575f768a44b
Lori Garzio <lgarzio@marine.rutgers.edu>
2021-08-10 10:27:12 -0400

    add keyword arguments

--- delayed_analysis/download_ds.py
+++ delayed_analysis/download_ds.py
@@ -2,7 +2,7 @@
 
 """
 Author: Lori Garzio on 4/2/2021
-Last modified: 6/18/2021
+Last modified: 8/10/2021
 Download a user-specified netCDF dataset from RUCOOL's glider ERDDAP server and save to a local directory
 """
 
@@ -16,7 +16,10 @@ def main(deploy, sdir):
     glider_vars = ['latitude', 'longitude', 'depth', 'conductivity', 'salinity', 'sci_water_pressure',
                    'temperature', 'sbe41n_ph_ref_voltage', 'chlorophyll_a', 'oxygen_concentration', 'water_depth',
                    'profile_lat', 'profile_lon', 'profile_time']
-    ds = cf.get_erddap_dataset(ru_server, deploy, glider_vars)
+
+    kwargs = dict()
+    kwargs['variables'] = glider_vars
+    ds = cf.get_erddap_dataset(ru_server, deploy, **kwargs)
     fname = f'{deploy}.nc'
     ds.to_netcdf(os.path.join(sdir, fname))
 
