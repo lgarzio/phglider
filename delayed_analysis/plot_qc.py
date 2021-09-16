0684d030dcd4a7fa339eb6c5a1388a1d7dda2a19
Lori Garzio <lgarzio@marine.rutgers.edu>
2021-09-16 15:33:35 -0400

    modify plotting for qc

--- delayed_analysis/plot_qc.py
+++ delayed_analysis/plot_qc.py
@@ -2,7 +2,7 @@
 
 """
 Author: Lori Garzio on 5/13/2021
-Last modified: 5/18/2021
+Last modified: 8/10/2021
 Plot delayed-mode pH glider data variables for the purposes of QC: seawater conductivity, temperature, salinity,
 chlorophyll, dissolved oxygen, pH reference voltage, and pH
 """
@@ -11,14 +11,17 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 import os
-import datetime as dt
 import matplotlib.pyplot as plt
 import functions.common as cf
 import functions.plotting as pf
 plt.rcParams.update({'font.size': 12})
 
 
-def main(fname, num_yos, dtrange):
+def main(fname, num_yos, dtrange, plot_xsection=None, plot_profiles=None, plot_qcvars=None):
+    plot_xsection = plot_xsection or True
+    plot_profiles = plot_profiles or True
+    plot_qcvars = plot_qcvars or False
+
     ds = xr.open_dataset(fname)
     ds = ds.sortby(ds.time)
     if dtrange:
@@ -41,8 +44,8 @@ def main(fname, num_yos, dtrange):
     # plot
     plt_vars = cf.plot_vars()
 
-    sdir_xsection = os.path.join(os.path.dirname(fname), 'qc_figs', f'{ds_t0savestr}_{ds_t1savestr}', 'xsections')
-    sdir_profile = os.path.join(os.path.dirname(fname), 'qc_figs', f'{ds_t0savestr}_{ds_t1savestr}', 'profiles')
+    sdir_xsection = os.path.join(os.path.dirname(fname), 'qc_figs_ph', f'{ds_t0savestr}_{ds_t1savestr}', 'xsections')
+    sdir_profile = os.path.join(os.path.dirname(fname), 'qc_figs_ph', f'{ds_t0savestr}_{ds_t1savestr}', 'profiles')
     os.makedirs(sdir_xsection, exist_ok=True)
     os.makedirs(sdir_profile, exist_ok=True)
 
@@ -82,38 +85,58 @@ def main(fname, num_yos, dtrange):
                 continue
             # plot xsection
             if len(variable) > 1:
-                fig, ax = plt.subplots(figsize=(12, 8))
-                figttl_xsection = f'{glider} {info["ttl"].split(" (")[0]}\n{t0str} to {t1str}'
-                xargs = dict()
-                xargs['clabel'] = info['ttl']
-                xargs['title'] = figttl_xsection
-                xargs['date_fmt'] = '%m-%d\n%H:%M'
-                xargs['grid'] = True
-                pf.xsection(fig, ax, tds.time.values, tds.depth.values, variable.values, **xargs)
+                if plot_xsection:
+                    fig, ax = plt.subplots(figsize=(12, 8))
+                    figttl_xsection = f'{glider} {info["ttl"].split(" (")[0]}\n{t0str} to {t1str}'
+                    xargs = dict()
+                    xargs['clabel'] = info['ttl']
+                    xargs['title'] = figttl_xsection
+                    xargs['date_fmt'] = '%m-%d\n%H:%M'
+                    xargs['grid'] = True
+                    pf.xsection(fig, ax, tds.time.values, tds.depth.values, variable.values, **xargs)
 
-                sfilename = f'{glider}_xsection_{pv}_{t0_savestr}.png'
-                sfile = os.path.join(sdir_xsection, pv, sfilename)
-                plt.savefig(sfile, dpi=300)
-                plt.close()
+                    sfilename = f'{glider}_xsection_{pv}_{t0_savestr}.png'
+                    sfile = os.path.join(sdir_xsection, pv, sfilename)
+                    plt.savefig(sfile, dpi=300)
+                    plt.close()
 
-                # plot profiles
-                colors = plt.cm.rainbow(np.linspace(0, 1, len(div_idxs)))
-                fig, ax = plt.subplots(figsize=(8, 10))
-                figttl_profile = f'{glider} {info["ttl"].split(" (")[0]}\n{t0str} to {t1str}'
-                pargs = dict()
-                pargs['xlabel'] = info['ttl']
-                pargs['title'] = figttl_profile
-                pargs['grid'] = True
-                pf.profile_yos(fig, ax, ds[pv], div_idxs, colors, **pargs)
+                if plot_profiles:
+                    # plot profiles
+                    colors = plt.cm.rainbow(np.linspace(0, 1, len(div_idxs)))
+                    fig, ax = plt.subplots(figsize=(8, 10))
+                    figttl_profile = f'{glider} {info["ttl"].split(" (")[0]}\n{t0str} to {t1str}'
+                    pargs = dict()
+                    pargs['xlabel'] = info['ttl']
+                    pargs['title'] = figttl_profile
+                    pargs['grid'] = True
+                    pf.profile_yos(fig, ax, ds[pv], div_idxs, colors, **pargs)
+                    sfilename = f'{glider}_profile_{pv}_{t0_savestr}.png'
+                    if plot_qcvars:
+                        if 'ph_total' in pv:
+                            ph_qcvars = ['qartod_{}_gross_range_flag'.format(pv), 'qartod_{}_spike_flag'.format(pv)]
+                            for pqc in ph_qcvars:
+                                ax.plot()
+                                tds[pv][np.where(tds[pqc] == 1)] = np.nan  # turn "good" values to nan
+                                if np.sum(~np.isnan(tds[pv].values)) > 0:  # if there are any values not "good", plot them red
+                                    xmask = ~np.isnan(tds[pv])
+                                    ax.scatter(tds[pv][xmask], tds.depth.values[xmask], marker='x', color='k', s=60, label=pqc)
+                                    ax.legend()
+                                    if sfilename.startswith('qc-'):
+                                        continue
+                                    else:
+                                        sfilename = f'qc-{sfilename}'
 
-                sfilename = f'{glider}_profile_{pv}_{t0_savestr}.png'
-                sfile = os.path.join(sdir_profile, pv, sfilename)
-                plt.savefig(sfile, dpi=300)
-                plt.close()
+                    sfile = os.path.join(sdir_profile, pv, sfilename)
+                    plt.savefig(sfile, dpi=300)
+                    plt.close()
 
 
 if __name__ == '__main__':
-    ncfile = '/Users/garzio/Documents/rucool/Saba/gliderdata/2021/ru30-20210226T1647/delayed/ru30-20210226T1647-profile-sci-delayed_shifted.nc'
-    number_yos = 20  # number of yos (down-up profile pairs) to plot in one image
-    date_range = None  # [dt.datetime(2021, 3, 1), dt.datetime(2021, 3, 5)]  # None
-    main(ncfile, number_yos, date_range)
+    ncfile = '/Users/garzio/Documents/rucool/Saba/gliderdata/2021/ru30-20210716T1804/delayed/ru30-20210716T1804-profile-sci-delayed_shifted.nc'
+    number_yos = 6  # number of yos (down-up profile pairs) to plot in one image
+    date_range = None   # [dt.datetime(2021, 3, 16), dt.datetime(2021, 3, 18)]  # None
+    kwargs = {}
+    # kwargs['plot_xsection'] = False
+    # kwargs['plot_profiles'] = False
+    # kwargs['plot_qcvars'] = True
+    main(ncfile, number_yos, date_range, **kwargs)
