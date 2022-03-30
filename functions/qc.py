7844196832eda0ac2903e88b21c91545d66d7f3b
Lori Garzio <lgarzio@marine.rutgers.edu>
2022-03-30 19:23:17 -0400

    Update qc.py
    -remove gross range
    -fix location and spike tests

--- functions/qc.py
+++ functions/qc.py
@@ -2,7 +2,7 @@
 
 """
 Author: Lori Garzio on 4/5/2021
-Last modified: 8/10/2021
+Last modified: 3/29/2022
 Modified from MATLAB code written by Liza Wright-Fairbanks, Spring 2020.
 Based on IOOS manual for real-time quality control of pH data observations.
 """
@@ -10,78 +10,39 @@ Based on IOOS manual for real-time quality control of pH data observations.
 import numpy as np
 import pandas as pd
 import xarray as xr
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 pd.set_option('display.width', 320, "display.max_columns", 10)  # for display in pycharm console
 
 
-def assign_qartod_attrs(data_array, data_range, test):
-    data_array.attrs['actual_range'] = data_range
-    data_array.attrs['comment'] = 'QARTOD {} test'.format(test)
-    data_array.attrs['flag_meanings'] = 'GOOD NOT_EVALUATED SUSPECT BAD MISSING'
-    data_array.attrs['flag_values'] = '[1 2 3 4 9]'
-    data_array.attrs['ioos_category'] = 'Quality'
-    data_array.attrs['long_name'] = 'QARTOD {} test'.format(test)
-    data_array.attrs['valid_min'] = '1'
-    data_array.attrs['valid_max'] = '9'
-
-    return data_array
-
-
-def bad_profiles():
+def assign_qartod_attrs(test, varname, thresholds):
     """
-    Check for bad CTD profile pairs
+    Define the QARTOD QC variable attributes
+    :param test: QARTOD QC test
+    :param varname: sensor variable name (e.g. conductivity)
+    :param thresholds: flag thresholds from QC configuration files
     """
 
+    flag_meanings = 'GOOD NOT_EVALUATED SUSPECT FAIL MISSING'
+    flag_values = [1, 2, 3, 4, 9]
+    standard_name = f'{test}_quality_flag'  # 'gross_range_test_quality_flag'
+    long_name = f'{" ".join([x.capitalize() for x in test.split("_")])} Quality Flag'
 
-def gross_range(dataset, vname, sensor_lims, user_lims=None):
-    """
-    Test 4: QARTOD test for sensor and user pH value ranges
-    :param dataset: xarray dataset
-    :param vname: variable name (e.g. ph_total)
-    :param sensor_lims: minimum and maximum sensor limits (e.g. for pH min: 0, max: 14)
-    :param user_lims: optional user-defined minimum and maximum data limits (e.g. for pH min: 6.5, max : 9)
-    :return: dataset with the pH gross range flag variable added
-    """
-    val = dataset[vname]
-    gross_range_flag = np.full(len(val), 1, dtype='int8')
+    # Defining gross/flatline QC variable attributes
+    attrs = {
+        'standard_name': standard_name,
+        'long_name': long_name,
+        'flag_values': np.byte(flag_values),
+        'flag_meanings': flag_meanings,
+        'flag_configurations': str(thresholds),
+        'valid_min': np.byte(min(flag_values)),
+        'valid_max': np.byte(max(flag_values)),
+        'ioos_qc_module': 'qartod',
+        'ioos_qc_test': f'{test}',
+        'ioos_qc_target': varname,
+    }
 
-    # check for nans
-    nan_idx = np.isnan(val.values)
-    gross_range_flag[nan_idx] = 9
-
-    # user range
-    if user_lims:
-        user_idx = np.logical_or(val.values > user_lims['max'], val.values < user_lims['min'])
-        gross_range_flag[user_idx] = 3
-        add_com_user = 'SUSPECT: values < {} or > {}.'.format(user_lims['min'], user_lims['max'])
-
-    # sensor range
-    if np.logical_and(~np.isnan(sensor_lims['min']), ~np.isnan(sensor_lims['max'])):
-        sensor_idx = np.logical_or(val.values >= sensor_lims['max'], val.values <= sensor_lims['min'])
-        add_com_sen = 'BAD: values <= {} or >= {}.'.format(sensor_lims['min'], sensor_lims['max'])
-    elif ~np.isnan(sensor_lims['max']):
-        sensor_idx = val.values >= sensor_lims['max']
-        add_com_sen = 'BAD: values >= {}.'.format(sensor_lims['max'])
-    elif ~np.isnan(sensor_lims['min']):
-        sensor_idx = val.values <= sensor_lims['min']
-        add_com_sen = 'BAD: values <= {}.'.format(sensor_lims['min'])
-    gross_range_flag[sensor_idx] = 4
-
-    varname = 'qartod_{}_gross_range_flag'.format(val.name)
-    da = xr.DataArray(gross_range_flag, coords=val.coords, dims=val.dims, name=varname)
-    vmin, vmax = np.nanmin(gross_range_flag), np.nanmax(gross_range_flag)
-    if vmin == vmax:
-        vrange = '[{}]'.format(vmin)
-    else:
-        vrange = '[{} {}]'.format(vmin, vmax)
-    da = assign_qartod_attrs(da, vrange, 'gross range')
-    if user_lims:
-        com = '{}. {} {}'.format(da.attrs['comment'], add_com_user, add_com_sen)
-    else:
-        com = '{}. {}'.format(da.attrs['comment'], add_com_sen)
-    da.attrs['comment'] = com
-    dataset[varname] = da
-
-    return dataset
+    return attrs
 
 
 def location_test(dataset, coordlims):
@@ -91,34 +52,55 @@ def location_test(dataset, coordlims):
     :param coordlims: dictionary containing coordinate limits
     :return: dataset with lat/lon location flag variables added
     """
-    global_values = {'latitude': 90, 'longitude': 180}
-    locs = ['latitude', 'longitude']
-    for loc in locs:
-        val = dataset[loc]
-        location_flag = np.full(len(val), 1, dtype='int8')
+    global_lims = {'latitude': [-90, 90], 'longitude': [-180, 180]}
+    global_values = dict(latitude=[global_lims['latitude'][0], global_lims['latitude'][0],
+                                   global_lims['latitude'][1], global_lims['latitude'][1]],
+                         longitude=[global_lims['longitude'][0], global_lims['longitude'][1],
+                                    global_lims['longitude'][1], global_lims['longitude'][0]])
+    global_polygon = Polygon(list(zip(global_values['longitude'], global_values['latitude'])))
 
-        # check for nans
-        nan_idx = np.isnan(val.values)
-        location_flag[nan_idx] = 9
+    local_values = dict(latitude=[coordlims['latitude'][0], coordlims['latitude'][0],
+                                  coordlims['latitude'][1], coordlims['latitude'][1]],
+                        longitude=[coordlims['longitude'][0], coordlims['longitude'][1],
+                                   coordlims['longitude'][1], coordlims['longitude'][0]])
+    local_polygon = Polygon(list(zip(local_values['longitude'], local_values['latitude'])))
 
-        # global check
-        gl_idx = np.argwhere(abs(val.values) > global_values[loc])
-        location_flag[gl_idx] = 4
+    flag_vals = np.full(len(dataset.longitude), 2, dtype='int32')
 
-        # local check
-        idx = np.logical_or(val.values > coordlims[loc]['max'], val.values < coordlims[loc]['min'])
-        location_flag[idx] = 3
+    # check for missing values
+    nan_idx = np.logical_or(np.isnan(dataset.longitude.values), np.isnan(dataset.latitude.values))
+    flag_vals[nan_idx] = 9
 
-        # made a data array for the qc flag and add it to the dataset
-        varname = 'qartod_{}_location_flag'.format(loc)
-        da = xr.DataArray(location_flag, coords=val.coords, dims=val.dims, name=varname)
-        vmin, vmax = np.nanmin(location_flag), np.nanmax(location_flag)
-        if vmin == vmax:
-            vrange = '[{}]'.format(vmin)
+    # make sure each lat/lon pair is within global and local ranges
+    for i, lon in enumerate(dataset.longitude.values):
+        lat = dataset.latitude.values[i]
+
+        if not global_polygon.contains(Point(lon, lat)):
+            flag_vals[i] = 4
+        elif not local_polygon.contains(Point(lon, lat)):
+            flag_vals[i] = 3
         else:
-            vrange = '[{} {}]'.format(vmin, vmax)
-        da = assign_qartod_attrs(da, vrange, 'location')
-        dataset[varname] = da
+            flag_vals[i] = 1
+
+    # made a data array for the qc flag and add it to the dataset
+    varname = 'location_qartod_test'
+    vmin, vmax = np.nanmin(flag_vals), np.nanmax(flag_vals)
+    if vmin == vmax:
+        vrange = '[{}]'.format(vmin)
+    else:
+        vrange = '[{} {}]'.format(vmin, vmax)
+
+    thresholds = dict(suspect=coordlims,
+                      fail=global_lims)
+
+    attrs = assign_qartod_attrs('location', 'latitude longitude', thresholds)
+
+    da = xr.DataArray(flag_vals, coords=dataset.longitude.coords, dims=dataset.longitude.dims, name=varname,
+                      attrs=attrs)
+    da.attrs['ancillary_variables'] = 'latitude longitude'
+    da.attrs['actual_range'] = vrange
+
+    dataset[varname] = da
 
     return dataset
 
@@ -128,52 +110,76 @@ def spike_test(dataset, vname, thresholds):
     Test 6: QARTOD spike test
     :param dataset: xarray dataset
     :param vname: variable name (e.g. ph_total)
-    :param thresholds: user-defined low and high thresholds for spikes
+    :param thresholds: user-defined suspect and fail thresholds for spikes, and time threshold in seconds
     :return: dataset with lat/lon location flag variables added
     """
     print('\nRunning spike test on {}'.format(vname))
-    val = dataset[vname]
-    #val = val[0:50]  # for debugging
-    spike_flag = np.empty(len(val))
-    spike_flag[:] = np.nan
+    qc_varname = f'{vname}_qartod_spike_test'
 
-    # TODO - check number of nans in spike_ref
+    # grab data for sensor
+    data = dataset[vname]
+    # identify where not nan
+    non_nan_ind = np.invert(np.isnan(data))
+    # non_nan_ind[50:] = False  # for debugging
 
-    # define spike reference as mean of 10 surrounding points
-    # or if at the very beginning(end) of the array, spike reference is the mean of the 10 subsequent(previous) points
-    for i, value in enumerate(val.values):
-        #print('{} of {}'.format(i, len(val)-1))
+    data_nonan = data[non_nan_ind]
+
+    flag_vals = 2 * np.ones(np.shape(data))
+    flag_vals[np.invert(non_nan_ind)] = 9
+
+    # define spike reference as median of 10 surrounding points
+    # or if at the very beginning(end) of the array, spike reference is the median of the 10 subsequent(previous) points
+    test_vals = 2 * np.ones(np.shape(data_nonan))
+    for i, value in enumerate(data_nonan):
         if i < 5:
-            spike_ref = np.nanmean(val.values[(i + 1):(i + 11)])
-        elif i > len(val) - 6:
-            spike_ref = np.nanmean(val.values[(i - 10):i])
+            data_ref = data_nonan[(i + 1):(i + 11)]
+        elif i > len(data_nonan) - 6:
+            data_ref = data_nonan[(i - 10):i]
         else:
-            spike_ref = np.nanmean(np.append(val.values[(i - 5):i], val.values[(i + 1):(i + 6)]))
+            data_ref = xr.concat([data_nonan[(i - 5):i], data_nonan[(i + 1):(i + 6)]], dim='time')
+
+        time_diff_seconds = np.diff(data_ref.time).astype('timedelta64[s]').astype(float)
+        if np.max(time_diff_seconds) > thresholds['seconds']:
+            # if the time interval is longer than the defined threshold,
+            # try taking the median of the 4 surrounding data points
+            if i < 2:
+                data_ref = data_nonan[(i + 1):(i + 5)]
+            elif i > len(data_nonan) - 3:
+                data_ref = data_nonan[(i - 4):i]
+            else:
+                data_ref = xr.concat([data_nonan[(i - 2):i], data_nonan[(i + 1):(i + 3)]], dim='time')
+
+            time_diff_seconds = np.diff(data_ref.time).astype('timedelta64[s]').astype(float)
+            if np.max(time_diff_seconds) > thresholds['seconds']:
+                # if the time interval is still longer than the defined threshold,
+                # try taking the median of the 4 subsequent points
+                data_ref = data_nonan[(i + 1):(i + 5)]
+                time_diff_seconds = np.diff(data_ref.time).astype('timedelta64[s]').astype(float)
+                if np.max(time_diff_seconds) > thresholds['seconds']:
+                    # try taking the median of the 4 subsequent or previous points
+                    data_ref = data_nonan[(i - 4):i]
+                    time_diff_seconds = np.diff(data_ref.time).astype('timedelta64[s]').astype(float)
+                    if np.max(time_diff_seconds) > thresholds['seconds']:
+                        # if the time interval is still longer than the defined threshold, leave flag values as
+                        # 2/NOT_EVALUATED
+                        continue
+
+        spike_ref = np.median(data_ref)
 
         # check the difference vs thresholds
-        diff = abs(spike_ref - value)
-        if np.isnan(diff):  # check for nans
-            qc = 9
-        elif diff > thresholds['high']:
-            qc = 4
-        elif diff > thresholds['low']:
-            qc = 3
+        diff = abs(spike_ref - value.values)
+        if diff > thresholds['fail']:
+            qc_value = 4
+        elif diff > thresholds['suspect']:
+            qc_value = 3
         else:
-            qc = 1
-        spike_flag[i] = qc
+            qc_value = 1
+        test_vals[i] = qc_value
 
-    spike_flag = spike_flag.astype('int8')
-    varname = 'qartod_{}_spike_flag'.format(val.name)
-    da = xr.DataArray(spike_flag, coords=val.coords, dims=val.dims, name=varname)
-    vmin, vmax = np.nanmin(spike_flag), np.nanmax(spike_flag)
-    if vmin == vmax:
-        vrange = '[{}]'.format(vmin)
-    else:
-        vrange = '[{} {}]'.format(vmin, vmax)
-    da = assign_qartod_attrs(da, vrange, 'spike')
-    da.attrs['comment'] = '{}. SUSPECT = threshold {}. BAD = threshold {}'.format(da.attrs['comment'],
-                                                                                  thresholds['low'],
-                                                                                  thresholds['high'])
-    dataset[varname] = da
+    # insert test results in the original data array with missing values
+    flag_vals[non_nan_ind] = test_vals
 
-    return dataset
+    attrs = assign_qartod_attrs('spike_test', vname, thresholds)
+    da = xr.DataArray(flag_vals.astype('int32'), coords=data.coords, dims=data.dims, attrs=attrs, name=qc_varname)
+
+    return da
