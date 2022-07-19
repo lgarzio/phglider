7db9019546c91505e282cdcbea981d1bd22aa8ec
Lori Garzio <lgarzio@marine.rutgers.edu>
2022-07-19 15:52:26 -0400

    Update glider_to_dac.py

--- delayed_analysis/glider_to_dac.py
+++ delayed_analysis/glider_to_dac.py
@@ -2,7 +2,7 @@
 
 """
 Author: Lori Garzio on 4/28/2021
-Last modified: 4/5/2021
+Last modified: 7/19/2022
 Process final glider dataset to upload to the IOOS glider DAC (https://gliders.ioos.us/).
 Modified from code written by Leila Belabbassi.
 """
@@ -18,25 +18,25 @@ pd.set_option('display.width', 320, "display.max_columns", 10)  # for display in
 
 
 def main(fname):
-    deploy = '-'.join(fname.split('/')[-1].split('-')[0:2])
-    proc_vars = ['unix_time', 'pressure', 'temperature', 'salinity', 'chlorophyll_a',
-                 'total_alkalinity', 'saturation_aragonite', 'latitude', 'longitude', 'conductivity',
-                 'sbe41n_ph_ref_voltage_shifted', 'ph_total_shifted', 'oxygen_concentration_shifted',
-                 'oxygen_concentration_shifted_mgL']
-    ds = xr.open_dataset(fname)
+    savedir = os.path.join(os.path.dirname(fname), 'ngdac')
+    os.makedirs(savedir, exist_ok=True)
 
-    # add unix time
-    dates = pd.to_datetime(ds.time.values)
-    unix = (dates - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
-    da = xr.DataArray(unix, coords=ds.time.coords, dims=ds.time.dims, name='unix_time')
-    ds['unix_time'] = da
+    deploy = '-'.join(fname.split('/')[-1].split('-')[0:2])
+    proc_vars = ['time', 'profile_time', 'profile_lon', 'profile_lat', 'latitude', 'longitude', 'depth', 'pressure',
+                 'temperature', 'density', 'salinity', 'chlorophyll_a', 'total_alkalinity',
+                 'aragonite_saturation_state', 'conductivity',
+                 'sbe41n_ph_ref_voltage', 'sbe41n_ph_ref_voltage_shifted', 'ph_total', 'ph_total_shifted',
+                 'oxygen_concentration', 'oxygen_concentration_shifted', 'oxygen_saturation',
+                 'oxygen_saturation_shifted', 'oxygen_concentration_mgL', 'oxygen_concentration_shifted_mgL',
+                 'pressure_interpolated', 'temperature_interpolated', 'salinity_interpolated']
+    ds = xr.open_dataset(fname)
 
     # find deployment configuration files
     ga, va, inst = cf.find_configs(deploy)
     with open(va) as json_file:
         var_attrs = json.load(json_file)
 
-    # add additional attribute information to total_alkalinity and pH
+    # add additional attribute information to total_alkalinity and instrument_pH
     var_attrs['total_alkalinity']['comment'] = ds.total_alkalinity.comment
     phsensorcals = ds.polynomial_coefficients.calibration_coefficients
     var_attrs['ph_total_shifted']['comment'] = ' sensor cals: '.join((var_attrs['ph_total_shifted']['comment'],
@@ -67,7 +67,7 @@ def main(fname):
     data_dict.update({'dims': {'time': {'dims': 'time', 'data': len(proc_vars) - 1}}})
 
     # add coordinates
-    data_dict.update({'coords': {'time': {'dims': 'time', 'data': ds['unix_time'].values}}})
+    data_dict.update({'coords': {'time': {'dims': 'time', 'data': ds['time'].values}}})
 
     # convert to dataset
     ds = xr.Dataset.from_dict(data_dict)
@@ -80,17 +80,35 @@ def main(fname):
         ds[key] = np.nan
         ds[key].attrs = values
 
-    # rename variables so they match the previous versions that were sent to the DAC
-    rename_dict = {'unix_time': 'UnixTime', 'pressure': 'Pressure', 'temperature': 'Temperature',
-                   'salinity': 'Salinity', 'chlorophyll_a': 'Chlorophyll', 'total_alkalinity': 'TotalAlkalinity',
-                   'saturation_aragonite': 'AragoniteSaturationState', 'latitude': 'Latitude',
-                   'longitude': 'Longitude', 'conductivity': 'Conductivity',
-                   'sbe41n_ph_ref_voltage_shifted': 'pHReferenceVoltage', 'ph_total_shifted': 'pH',
-                   'oxygen_concentration_shifted': 'Oxygen_molar', 'oxygen_concentration_shifted_mgL': 'Oxygen_mgL'}
+    # add sensor calibration information to instrument_ph
+    ds['instrument_ph'].attrs['calibration_coefficients'] = f'pH sensor calibration coefficents: {phsensorcals}'
+
+    # rename variables
+    rename_dict = {'latitude': 'lat', 'longitude': 'lon',
+                   'sbe41n_ph_ref_voltage': 'pH_reference_voltage_raw',
+                   'sbe41n_ph_ref_voltage_shifted': 'pH_reference_voltage_corrected',
+                   'ph_total': 'pH_raw',
+                   'ph_total_shifted': 'pH_corrected',
+                   'oxygen_concentration': 'oxygen_concentration_raw',
+                   'oxygen_concentration_shifted': 'oxygen_concentration_corrected',
+                   'oxygen_saturation': 'oxygen_saturation_raw',
+                   'oxygen_saturation_shifted': 'oxygen_saturation_corrected',
+                   'oxygen_concentration_mgL': 'oxygen_concentration_mgl_raw',
+                   'oxygen_concentration_shifted_mgL': 'oxygen_concentration_mgl_corrected'}
     ds = ds.rename(rename_dict)
 
-    nc_filename = '{}/{}_to_dac.nc'.format(os.path.dirname(fname), deploy)
-    ds.to_netcdf(nc_filename)
+    # split up the dataset into profile files
+    ds = ds.swap_dims({'time': 'profile_time'})
+    for pt in np.unique(ds.profile_time.values):
+        dspt = ds.sel(profile_time=pt)
+        try:
+            dspt = dspt.swap_dims({'profile_time': 'time'})
+            dspt = dspt.reset_coords(names='profile_time')
+            nc_fname = '{}_{}_delayed.nc'.format(deploy.split('-')[0], pd.to_datetime(pt).strftime('%Y%m%dT%H%MZ'))
+            nc_filename = os.path.join(savedir, nc_fname)
+            dspt.to_netcdf(nc_filename)
+        except ValueError:
+            continue
 
 
 if __name__ == '__main__':
