c27820c33977fd26712e3d6b4444e74de8e6d5e0
Lori Garzio <lgarzio@marine.rutgers.edu>
2022-07-21 13:51:06 -0400

    Update common.py
    update TA/salinity reference

--- functions/common.py
+++ functions/common.py
@@ -102,8 +102,8 @@ def calculate_ta(deployment, salinity):
     ta.attrs['observation_type'] = 'calculated'
     ta.attrs['comment'] = 'Calculated from salinity using the linear relationship (TA = {} * salinity + {}) ' \
                           'determined from in-situ water sampling data taken during glider deployment and ' \
-                          'recovery in addition to ship-based water samples as described in Saba ' \
-                          'et al 2019 https://doi.org/10.3389/fmars.2019.00664'.format(np.round(ta_equ['m'], 2),
+                          'recovery in addition to ship-based water samples as described in Wright-Fairbanks ' \
+                          'et al 2020 https://doi.org/10.1029/2020JC016505'.format(np.round(ta_equ['m'], 2),
                                                                                        np.round(ta_equ['b'], 2))
 
     return ta
